9301c062c9ef3c77e695609e512c0c1605e272e0
valeria <golovatova.vm@gmail.com>
2026-04-22 13:52:19 +0700

    Files were changed, unnecessary parameters were removed

--- Abstract-Factory/MethodUnit.h
+++ Abstract-Factory/MethodUnit.h
@@ -27,23 +27,3 @@ private:
 
 
 
-
-std::string compile( unsigned int level = 0 ) const {
-    std::string result = generateShift( level );
-    if( m_flags & STATIC ) {
-        result += "static ";
-    } else if( m_flags & VIRTUAL ) {
-        result += "virtual ";
-    }
-    result += m_returnType + " ";
-    result += m_name + "()";
-    if( m_flags & CONST ) {
-        result += " const";
-    }
-    result += " {\n";
-    for( const auto& b : m_body ) {
-        result += b->compile( level + 1 );
-    }
-    result += generateShift( level ) + "}\n";
-    return result;
-}
